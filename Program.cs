c2ccfa9dd20a1e01f00aeb8b32244495e3f8fcdc
YeniMargot20 <yenihuamanhuillca@gmail.com>
2025-06-05 23:27:51 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JornadadeTrabajo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int h, v, salario = 0;
+            int jornada = 48;
+            string nombre;
+            Console.WriteLine("Ingresar el nombre del trabajador");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingresar la cantidad de horas trabajadas");
+            h = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresar el valor por hora trabajada");
+            v = int.Parse(Console.ReadLine());
+            salario = h * v;
+            Console.WriteLine("El salario del trabajador: ");
+            Console.WriteLine(nombre + "es" + salario + "\n");
+            Console.WriteLine("La jornada es: " + jornada + "horas: ");
+
+            int he = h - jornada;
+            if (h > jornada)
+            {
+                Console.WriteLine("Tiene: " + he + "horas extras: ");
+            }
+            else
+            {
+                Console.WriteLine("No tiene horas extras: ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
